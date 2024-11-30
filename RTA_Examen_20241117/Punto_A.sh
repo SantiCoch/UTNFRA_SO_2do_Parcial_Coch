47a213eef11fb35db6862135726a8209baeff49f
SantiCoch <santicoch195@gmail.com>
2024-11-30 17:04:21 -0500

    Punto A Realizado

--- RTA_Examen_20241117/Punto_A.sh
+++ RTA_Examen_20241117/Punto_A.sh
@@ -1,23 +1,34 @@
-#!/bin/bash
 
-sudo pvcreate /dev/sda /dev/sdb
 
-sudo vgcreate vg_datos /dev/sda
-sudo vgextend vg_datos /dev/sdb
+sudo pvcreate /dev/sdc1 /dev/sdb1 /dev/sdb2
+sudo pvs
+sudo vgcreate vg_datos /dev/sdc1
+sudo vgcreate vg_temp /dev/sdb1 /dev/sdb2
+sudo pvs
 
 sudo lvcreate -n lv_docker -L 5M vg_datos
 sudo lvcreate -n lv_workareas -L 1.5G vg_datos
 sudo lvcreate -n lv_swap -L 512M vg_temp
+sudo lvs
 
-sudo mkfs.ext4 /dev/vg_datos/lv_docker
-sudo mkfs.ext4 /dev/vg_datos/lv_workareas
-sudo mkswap /dev/vg_temp/lv_swap
+sudo mkswap /dev/mapper/vg_temp-lv_swap
 
-sudo mount /dev/vg_datos/lv_docker /var/lib/docker
+sudo swapon /dev/mapper/vg_temp-lv_swap
+sudo swapon -s
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
+sudo lsblk -l
+
+sudo ls -l /var/lib/docker/
+
+sudo mkdir -p /work
 sudo mount /dev/vg_datos/lv_workareas /work
-sudo swapon /dev/vg_temp/lv_swap
+sudo mount /dev/vg_datos/lv_docker /var/lib/docker
+df -h
 
 echo '/dev/vg_datos/lv_docker /var/lib/docker ext4 defaults 0 0' | sudo tee -a /etc/fstab
 echo '/dev/vg_datos/lv_workareas /work ext4 defaults 0 0' | sudo tee -a /etc/fstab
 echo '/dev/vg_temp/lv_swap none swap sw 0 0' | sudo tee -a /etc/fstab
 
+sudo systemctl restart docker
+sudo systemctl status docker
--- RTA_Examen_20241117/Punto_C.sh
+++ RTA_Examen_20241117/Punto_C.sh
@@ -1,20 +1,12 @@
 
-# Este archivo define como se construye la imagen de Docker.
 FROM nginx
 COPY index.html /usr/share/nginx/html/index.html
 
-# Editamos el archivo html
-nano html
-
-
-# Creamos el script run.sh
-
-# Construye la imagen
 docker build -t web1-coch .
-# Etiqueta la imagen con el nombre de usuario
+
 docker tag web1-coch santicoch810/web1-coch
-# Realiza un push de la imagen a Docker Hub
+
 docker push santicoch810/web1-coch
-# Ejecuta la imagen como un contenedor en el puerto 8080 del host
+
 docker run -d -p 8080:80 santicoch810/web1-coch
 
